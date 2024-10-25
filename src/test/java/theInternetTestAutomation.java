d2df16de80d3c664322bb2aeec21c27a6e5208f6
Abdulmalik Muhammad <110231662+malikthedsigner@users.noreply.github.com>
2024-10-25 22:05:17 +0100

    Find Element and Send Keys

--- src/test/java/theInternetTestAutomation.java
+++ src/test/java/theInternetTestAutomation.java
@@ -1,18 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class seleniumClassOne {
+public class theInternetTestAutomation {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://google.com");
-        driver.navigate().to("https://facebook.com");
-        String pageTitle = driver.getTitle();
-        System.out.println(pageTitle);
-        driver.navigate().back();
-        Thread.sleep(5000);
-        driver.quit();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
     }
 }
